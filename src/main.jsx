9659e0ba89aaf5810dfb6d8cfe63e8ffa36df5ec
Paul Barden <paulbarden2376@gmail.com>
2025-10-27 11:03:55 -0600

    init rf

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import GlobeSystem from './systems/multi-globe/GlobeSystem.jsx'
-import { LoadingScreen } from './components/LoadingScreen.jsx'
 import './index.css'
 
-// Feature flag: set to true to use multi-globe system, false for original App
-const USE_MULTI_GLOBE = true
-
 function Main() {
-  const [isLoading, setIsLoading] = useState(true)
-
-  return (
-    <>
-      {isLoading && <LoadingScreen onLoadComplete={() => setIsLoading(false)} />}
-
-      {!isLoading && (
-        USE_MULTI_GLOBE ? (
-          <GlobeSystem onReady={() => console.log('Multi-globe system ready')} />
-        ) : (
-          <App />
-        )
-      )}
-    </>
-  )
+  return <App />
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
